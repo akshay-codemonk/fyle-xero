b7f9b4cecdf6fa5be36a570bc3afecf69918707d
sanjeev29 <sanjeev.raichur@codemonk.in>
2019-12-17 12:18:26 +0530

    Add: Expenses and expense details UI
    - Add '<group_id>/expenses/' url to show expenses grouped under a particular expense group id
    - Add expense details modal to show detailed view of each expense present in expense group
    - ExpenseView renders expenses screen

--- apps/expense/urls.py
+++ apps/expense/urls.py
@@ -3,8 +3,9 @@ expense app URL Configuration
 """
 from django.urls import path
 
-from apps.expense.views import ExpenseGroupView
+from apps.expense.views import ExpenseGroupView, ExpenseView
 
 urlpatterns = [
     path('', ExpenseGroupView.as_view(), name="expense_groups"),
+    path('<group_id>/expenses/', ExpenseView.as_view(), name='expenses'),
 ]
--- apps/expense/views.py
+++ apps/expense/views.py
@@ -6,8 +6,19 @@ class ExpenseGroupView(View):
     """
     Expense Group View
     """
-    template_name = "expense/expense.html"
+    template_name = "expense/expense_group.html"
 
     def get(self, request, workspace_id):
         context = {"expense_groups_tab": "active", "expense_groups": "active"}
+        return render(request, self.template_name, context)
+
+
+class ExpenseView(View):
+    """
+    Expense View
+    """
+    template_name = "expense/expense.html"
+
+    def get(self, request, workspace_id, group_id):
+        context = {"expense_groups_tab": "active"}
         return render(request, self.template_name, context)
