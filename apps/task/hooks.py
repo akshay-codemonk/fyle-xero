852a5ae5263a7a239950317d911ab5c2a3966c00
sanjeev29 <sanjeev.raichur@codemonk.in>
2020-01-13 10:58:20 +0530

    Add: docstrings to tasks and hooks

--- apps/task/hooks.py
+++ apps/task/hooks.py
@@ -4,6 +4,11 @@ from apps.expense.models import ExpenseGroup
 
 
 def update_fetch_expense_task(task):
+    """
+    Update task log fields of "Fetching Expenses" task
+    :param task: Task object
+    """
+
     task_log = TaskLog.objects.get(task_id=task.id)
     if task.success:
         task_log.level = 'Success'
@@ -16,6 +21,11 @@ def update_fetch_expense_task(task):
 
 
 def update_create_invoice_task(task):
+    """
+    Update task log fields of "Creating Invoice" task
+    :param task: Task object
+    """
+
     expense_group_id = task.result
     expense_group = ExpenseGroup.objects.get(id=expense_group_id)
     task_log = TaskLog.objects.create(
