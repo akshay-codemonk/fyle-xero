4d7d94566b97294d3f5fb462afb8239f728894ab
sanjeev29 <sanjeev.raichur@codemonk.in>
2020-03-18 16:11:30 +0530

    Remove: Cors related middleware

--- fyle_xero_integration_web_app/settings.py
+++ fyle_xero_integration_web_app/settings.py
@@ -60,10 +60,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
